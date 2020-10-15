99c1b46766ead4ca9171ffff07fb47130c36f733
ablades <audaris@gmail.com>
2020-10-15 00:20:35 -0500

    token doesnt error

--- api/handlers/authcallback.go
+++ api/handlers/authcallback.go
@@ -1,14 +1,15 @@
 package handlers
 
 import (
+	"context"
 	"fmt"
-	"io/ioutil"
 	"log"
 	"net/http"
 
 	"github.com/ablades/relevant/config"
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
+	"github.com/spf13/viper"
 	"golang.org/x/oauth2"
 )
 
@@ -18,31 +19,41 @@ func (h *Handler) AuthCallback(c echo.Context) (err error) {
 	code := c.QueryParam("code")
 	queryState := c.QueryParam("state")
 	state := c.Get(middleware.DefaultCSRFConfig.ContextKey)
-	// set context
-	ctx := c.Request().Context()
+
 	// csrf check
 	if queryState != state {
 		return c.String(http.StatusForbidden, "csrf detected")
 	}
 
 	authConfig := config.GetAuthConfig()
+	// token, err := authConfig.Exchange(ctx, code)
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+
+	// Custom http client
+	client := &http.Client{
+		Transport: &oauth2.Transport{
+			// configure token
+			Source: authConfig.TokenSource(oauth2.NoContext, &oauth2.Token{
+				AccessToken: code,
+			}),
+			Base: &transp{
+				config:    authConfig,
+				userAgent: fmt.Sprintf("relevant_for_reddit/0.0 (by /u/%s)", viper.GetString("reddit.username")),
+			},
+		},
+	}
+
+	ctx := context.WithValue(oauth2.NoContext, oauth2.HTTPClient, client)
+
 	token, err := authConfig.Exchange(ctx, code)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	// Custom http client
-	*h.client = http.Client{
-		Transport: &oauth2.Transport{
-			// configure token
-			Source: authConfig.TokenSource(ctx, token),
-			Base:   h,
-		},
-	}
-
-	resp, err := h.client.Get("https://oauth.reddit.com/api/v1/me.json")
-	fmt.Println(resp.Header.Get("User-Agent"))
-	content, err := ioutil.ReadAll(resp.Body)
-
-	return c.JSONBlob(http.StatusOK, content)
+	fmt.Print(token)
+	// fesp, err := h.client.Get("https://oauth.reddit.com/api/v1/me.json")
+	// content, err := ioutil.ReadAll(resp.Body)
+	return c.String(http.StatusOK, "hi")
 }
--- api/handlers/handlers.go
+++ api/handlers/handlers.go
@@ -1,32 +1,32 @@
 package handlers
 
 import (
-	"fmt"
 	"net/http"
 
-	"github.com/spf13/viper"
 	"go.mongodb.org/mongo-driver/mongo"
+	"golang.org/x/oauth2"
 )
 
 // Handler is used so that we can dependency inject different services into the database
 type Handler struct {
-	db     *mongo.Database
-	client *http.Client
-	tp     http.RoundTripper
+	db *mongo.Database
+}
+
+type transp struct {
+	config    *oauth2.Config
+	userAgent string
 }
 
 // RoundTrip sets headers
-func (h *Handler) RoundTrip(req *http.Request) (*http.Response, error) {
-	req.Header.Set(
-		"User-Agent",
-		"relevant_for_reddit/1.0 (by /u/"+viper.GetString("reddit.username")+")",
-	)
+func (t *transp) RoundTrip(req *http.Request) (*http.Response, error) {
+	req.Header.Set("User-Agent", t.userAgent)
+	//fmt.Sprintf("relevant_for_reddit/0.0 (by /u/%s)", viper.GetString("reddit.username")),
+	// 	fmt.Sprintf("Basic ")
 
-	req.Header.Set(
-		"Authorization",
-		"Basic "+viper.GetString("reddit.client")+":"+viper.GetString("reddit.Secret")+")",
-	)
-	fmt.Println("HERE")
+	req.SetBasicAuth(t.config.ClientID, t.config.ClientSecret)
+	// 	"Basic "+viper.GetString("reddit.client")+":"+viper.GetString("reddit.Secret")+")",
+	// )
+
 	return http.DefaultTransport.RoundTrip(req)
 }
 
@@ -34,7 +34,5 @@ func (h *Handler) RoundTrip(req *http.Request) (*http.Response, error) {
 func NewHandler(db *mongo.Database) *Handler {
 	return &Handler{
 		db,
-		http.DefaultClient,
-		http.DefaultTransport,
 	}
 }
