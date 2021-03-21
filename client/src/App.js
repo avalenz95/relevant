2c1d97f51399209e6ba6173666e5301f37e4e8e8
ablades <audaris@gmail.com>
2021-03-21 15:33:31 -0500

    update frontend env var

--- client/src/App.js
+++ client/src/App.js
@@ -7,7 +7,7 @@ import Dashboard from './components/Dashboard/Dashboard.js'
 import rootReducer from './reducers/index.js'
 
 
-const  ep = "http://localhost:8080"
+const  ep = process.env.REACT_APP_ENDPOINT
 
 const store = createStore(rootReducer, applyMiddleware(thunk))
 
